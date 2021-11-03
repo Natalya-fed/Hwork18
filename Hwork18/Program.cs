391ae382e0f479cc1cc8995de296f56969d2a5a5
Наталья Фёдорова <natalyafedorova5@gmail.com>
2021-11-03 17:36:22 +0300

    Добавьте файлы проекта.

---
+++ Hwork18/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hwork18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "([]{ })[]";
+            Stack<char> scoba = new Stack<char>();
+            foreach (var i in str)
+            {
+                if (i == '(' || i == '[' || i == '{')
+                {
+                    scoba.Push(i);
+                }
+                if (i == ')')
+                {
+                    if (scoba.Contains('('))
+                    {
+                        scoba.Pop();
+                    }
+                }
+                if (i == ']')
+                {
+                    if (scoba.Contains('['))
+                    {
+                        scoba.Pop();
+                    }
+                }
+                if (i == '}')
+                {
+                    if (scoba.Contains('{'))
+                    {
+                        scoba.Pop();
+                    }
+                }
+            }
+
+            if (scoba.Count == 0)
+            {
+                Console.WriteLine("Скобки расставлены верно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены не верно");
+            }
+            Console.ReadKey();
+        }
+    }
+}
